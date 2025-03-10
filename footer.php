692362214e5b7ae0ae05ad18e7d8634cbafa05aa
Krishal90 <141979269+Krishal90@users.noreply.github.com>
2025-03-10 13:39:21 +0545

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,40 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="contact.php">Contact</a>
+            <a href="shop.php">Shop</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>
+            <a href="orders.php">My orders</a>
+            <a href="cart.php">My cart</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i> +977 9812342211 </p>
+            <p> <i class="fas fa-envelope"></i> binaya@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> Kathmandu, Nepal</p>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>Linkedin</a>
+        </div>
+
+    </div>
+
+    <div class="credit">&copy; copyright @ <?php echo date('Y'); ?> by <span>Flower Shop</span> </div>
+
+</section>
---
+++ shop_db.sql
@@ -0,0 +1,239 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(100) NOT NULL,
+  `user_id` int(100) NOT NULL,
+  `pid` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `price` int(100) NOT NULL,
+  `quantity` int(100) NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`id`, `user_id`, `pid`, `name`, `price`, `quantity`, `image`) VALUES
+(129, 14, 16, 'lavendor rose', 13, 1, 'rose.jpg'),
+(130, 14, 18, 'red tulipa', 11, 1, 'red tulipa.jpg'),
+(131, 14, 15, 'cottage rose', 15, 1, 'cottage rose.jpg'),
+(132, 15, 13, 'pink rose', 10, 1, 'pink roses.jpg'),
+(133, 15, 15, 'cottage rose', 15, 1, 'cottage rose.jpg'),
+(134, 15, 16, 'lavendor rose', 13, 3, 'lavendor rose.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `id` int(100) NOT NULL,
+  `user_id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `number` varchar(12) NOT NULL,
+  `message` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `message`
+--
+
+INSERT INTO `message` (`id`, `user_id`, `name`, `email`, `number`, `message`) VALUES
+(13, 14, 'Binaya', 'binaya@gmail.com', '0987654321', 'hi, how are you?');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(100) NOT NULL,
+  `user_id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `number` varchar(12) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `method` varchar(50) NOT NULL,
+  `address` varchar(500) NOT NULL,
+  `total_products` varchar(1000) NOT NULL,
+  `total_price` int(100) NOT NULL,
+  `placed_on` varchar(50) NOT NULL,
+  `payment_status` varchar(20) NOT NULL DEFAULT 'pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `name`, `number`, `email`, `method`, `address`, `total_products`, `total_price`, `placed_on`, `payment_status`) VALUES
+(17, 14, 'binaya', '0987654321', 'binaya@gmail.com', 'credit card', 'flat no. 321, kathmandu, kathmandu, nepal - 654321', ', cottage rose (3) , pink bouquet (1) , yellow queen rose (1) ', 80, '11-Mar-2022', 'pending'),
+(18, 14, 'binaya', '1234567899', 'binaya@gmail.com', 'paypal', 'flat no. 321, kathmandu, kathmandu, nepal - 654321', ', yellow queen rose (1) , pink rose (2) ', 40, '11-Mar-2022', 'completed');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `details` varchar(500) NOT NULL,
+  `price` int(100) NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `details`, `price`, `image`) VALUES
+(13, 'pink rose', 'A pink rose symbolizes love, admiration, gratitude, and elegance. Its soft petals and sweet fragrance evoke beauty and romance', 12, 'pink roses.jpg'),
+(15, 'cottage rose', 'Cottage roses are charming, fragrant blooms that thrive in rustic gardens, offering a romantic, timeless beauty with soft colors..', 15, 'cottage rose.jpg'),
+(16, 'lavendor rose', 'Lavender roses symbolize enchantment and grace, with their soft purple hue conveying elegance, mystery, and a sense of calm.', 13, 'lavendor rose.jpg'),
+(17, 'yellow tulipa', 'Yellow tulips are vibrant flowers symbolizing happiness and cheerfulness, often associated with spring and bringing warmth to any garden.', 14, 'yellow tulipa.jpg'),
+(18, 'red tulipa', 'Red tulips symbolize love and passion, with their bold, vibrant color making them a popular choice for romantic gestures.', 11, 'red tulipa.jpg'),
+(19, 'pink bouquet', 'A pink bouquet symbolizes admiration, gratitude, and grace, offering a soft, romantic aesthetic perfect for expressing affection and appreciation.', 15, 'pink bouquet.jpg'),
+(20, 'pink queen rose', 'The Pink Queen rose represents elegance, grace, and admiration, with its soft, delicate petals making it a timeless symbol of beauty.', 24, 'pink queen rose.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `user_type` varchar(20) NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `user_type`) VALUES
+(10, 'admin ', 'admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
+(14, 'user A', 'user01@gmail.com', '698d51a19d8a121ce581499d7b701668', 'user'),
+(15, 'user B', 'user02@gmail.com', '698d51a19d8a121ce581499d7b701668', 'user');
+(16, 'Binaya', 'b123@gmail.com', '1692fcfff3e01e7ba8cffc2baadef5f5','user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wishlist`
+--
+
+CREATE TABLE `wishlist` (
+  `id` int(100) NOT NULL,
+  `user_id` int(100) NOT NULL,
+  `pid` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `price` int(100) NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `wishlist`
+--
+
+INSERT INTO `wishlist` (`id`, `user_id`, `pid`, `name`, `price`, `image`) VALUES
+(60, 14, 19, 'pink bouquet', 15, 'pink bouquet.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=135;
+
+--
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
